dc2b9a1f7c8be9009226bec7b8154bd098a5117d
tmthyDXTR <langmi89@gmail.com>
2020-07-05 18:22:35 +0200

    init

---
+++ Assets/Scripts/derFloorManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class derFloorManager : MonoBehaviour
+{
+    public GameObject slots;
+
+    [SerializeField]
+    public List<GameObject> slotsObjects;
+
+
+    public Sprite sprite_1;
+    public Sprite sprite_2;
+    public Sprite sprite_3;
+    public Sprite sprite_4;
+    public Sprite sprite_5;
+    public Sprite sprite_6;
+    public Sprite sprite_7;
+    public Sprite sprite_8;
+    public Sprite sprite_9;
+    public Sprite sprite_10;
+    public Sprite sprite_11;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite_1 = Resources.Load<Sprite>("sprites/1");
+        sprite_2 = Resources.Load<Sprite>("sprites/1");
+        sprite_3 = Resources.Load<Sprite>("sprites/3");
+        sprite_4 = Resources.Load<Sprite>("sprites/4");
+        sprite_5 = Resources.Load<Sprite>("sprites/5");
+        sprite_6 = Resources.Load<Sprite>("sprites/6");
+        sprite_7 = Resources.Load<Sprite>("sprites/420");
+        sprite_8 = Resources.Load<Sprite>("sprites/SPOILER_woobi");
+        sprite_9 = Resources.Load<Sprite>("sprites/lampe");
+        sprite_10 = Resources.Load<Sprite>("sprites/leiter");
+        sprite_11 = Resources.Load<Sprite>("sprites/lunte");
+
+        slots = GameObject.Find("Slots");
+
+        foreach (Transform child in slots.transform)
+        {
+            slotsObjects.Add(child.gameObject);            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/slotty.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slotty : MonoBehaviour
+{
+    static string symbol1 = "1";
+    static string symbol2 = "2";
+    static string symbol3 = "3";
+    static string symbol4 = "4";
+    static string symbol5 = "5";
+    static string symbol6 = "6";
+    static string symbol7 = "420";
+    static string symbol8 = "Wubi";
+    static string symbol9 = "Dubi";
+    static string symbol10 = "Leiter";
+    static string symbol11 = "Lampe";
+    static string symbol12 = "Lunte";
+
+
+
+    public Dictionary<string, int> dict = new Dictionary<string, int>()
+    {
+        {symbol1, 80 },
+        {symbol2, 80 },
+        {symbol3, 80 },
+        {symbol4, 80 },
+        {symbol5, 80 },
+        {symbol6, 80 },
+        {symbol7, 50 },
+        {symbol8, 40 },
+        {symbol9, 30 },
+        {symbol10, 20 },
+        {symbol11, 15 }
+        
+    };
+
+    private List<string> CreateString(Dictionary<string, int> dict)
+    {
+        var result = new List<string>();
+
+        foreach (var symbol in dict)
+        {
+            for (int i = 0; i < symbol.Value; i++)
+            {
+                result.Add(symbol.Key);
+            }
+        }
+
+        return result;
+    }
+
+    private string TakeRandomSymbol(List<string> symbolList)
+    {
+        int selected = UnityEngine.Random.Range(0, symbolList.Count);
+
+        dict[symbolList[selected]]--;
+
+
+        return symbolList[selected];
+    }
+       
+    //
+    // Then randomly select a value from that list:
+    //
+    //private int selected = values[Random.Range(0, values.Length)];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var stringList = CreateString(dict);
+        //foreach (var item in stringList)
+        //{
+        //    Debug.Log(item);
+        //}
+        for (int i = 0; i < 15; i++)
+        {
+            Debug.Log(TakeRandomSymbol(stringList));
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
